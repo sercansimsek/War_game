a96d7b299d368b915c01b277e7aeb1a6cc759430
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-04 11:12:53 +0100

    endpoint tests

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const getCards = document.querySelector(".get-cards");
+const draw = document.querySelector(".draw");
 
 const url = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/";
 
+let drawURL;
+
+let deckId;
 getCards.addEventListener("click", function () {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			deckId = data.deck_id;
+			drawURL = `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`;
+		});
 });
+
+function handleDraw() {
+	fetch(drawURL)
+		.then((res) => res.json())
+		.then((data) => console.log(data));
+}
+
+draw.addEventListener("click", handleDraw);
+
+console.log(deckId);
