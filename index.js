8cffb14f666d6697e15069085858eacf99dbde6d
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-06 22:03:32 +0100

    Win condition logic implemented

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
 let deckId;
+const cardsContainer = document.getElementById("cards");
+const newDeckBtn = document.getElementById("new-deck");
+const drawCardBtn = document.getElementById("draw-cards");
+const result = document.querySelector(".result");
 
 function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       deckId = data.deck_id;
     });
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick);
+newDeckBtn.addEventListener("click", handleClick);
 
-document.getElementById("draw-cards").addEventListener("click", () => {
+drawCardBtn.addEventListener("click", () => {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      let listHTML = "";
+      cardsContainer.children[0].innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+            `;
+      cardsContainer.children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
+            `;
 
-      data.cards.map((card) => {
-        listHTML += `
-					<li>
-						<img src=${card.image} alt=${card.code}>
-					</li>
-				`;
-      });
-
-      document.querySelector(".cards").innerHTML = listHTML;
+      result.innerText = determineCardWinner(data.cards[0], data.cards[1]);
     });
 });
+
+function determineCardWinner(card1, card2) {
+  const valueOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+  const card1ValueIndex = valueOptions.indexOf(card1.value);
+  const card2ValueIndex = valueOptions.indexOf(card2.value);
+
+  if (card1ValueIndex > card2ValueIndex) {
+    return "Computer wins!";
+  } else if (card1ValueIndex < card2ValueIndex) {
+    return "You win!";
+  } else {
+    return "War!";
+  }
+}
