17834fb4645d6d9154f47cc07ecbb5735d334493
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-06 21:38:25 +0100

    Card image placement

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 let deckId;
 
 function handleClick() {
-	fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data);
-			deckId = data.deck_id;
-		});
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      deckId = data.deck_id;
+    });
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick);
 
 document.getElementById("draw-cards").addEventListener("click", () => {
-	fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data);
-			let listHTML = "";
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let listHTML = "";
 
-			data.cards.map((card) => {
-				listHTML += `
+      data.cards.map((card) => {
+        listHTML += `
 					<li>
 						<img src=${card.image} alt=${card.code}>
 					</li>
 				`;
-			});
+      });
 
-			document.querySelector(".list").innerHTML = listHTML;
-		});
+      document.querySelector(".cards").innerHTML = listHTML;
+    });
 });
