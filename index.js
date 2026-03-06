0b359b909ce3b11dfaab0f9a4f20df9144e5081b
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-06 10:30:10 +0100

    2 random card drawed and displayed

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const getCards = document.querySelector(".get-cards");
-const draw = document.querySelector(".draw");
-
-const url = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/";
-
-let drawURL;
-
 let deckId;
-getCards.addEventListener("click", function () {
-	fetch(url)
-		.then((response) => response.json())
-		.then((data) => {
-			deckId = data.deck_id;
-			drawURL = `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`;
-		});
-});
 
-function handleDraw() {
-	fetch(drawURL)
+function handleClick() {
+	fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
 		.then((res) => res.json())
-		.then((data) => console.log(data));
+		.then((data) => {
+			console.log(data);
+			deckId = data.deck_id;
+		});
 }
 
-draw.addEventListener("click", handleDraw);
+document.getElementById("new-deck").addEventListener("click", handleClick);
 
-console.log(deckId);
+document.getElementById("draw-cards").addEventListener("click", () => {
+	fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			let listHTML = "";
+
+			data.cards.map((card) => {
+				listHTML += `
+					<li>
+						<img src=${card.image} alt=${card.code}>
+					</li>
+				`;
+			});
+
+			document.querySelector(".list").innerHTML = listHTML;
+		});
+});
